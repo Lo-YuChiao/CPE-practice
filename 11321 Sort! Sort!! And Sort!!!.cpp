c6ea85730f5719e24ac0800ca8b3fd5eca057efd
Lo-YuChiao <59047078+Lo-YuChiao@users.noreply.github.com>
2020-05-15 00:09:44 +0800

    Update 11321 Sort! Sort!! And Sort!!!.cpp

--- 11321 Sort! Sort!! And Sort!!!.cpp
+++ 11321 Sort! Sort!! And Sort!!!.cpp
@@ -1,50 +1,44 @@
 //
 //  main.cpp
-//  11321 Sort! Sort!! And Sort!!!
+//  11321 Sort! Sort!! and Sort!!!
 //
-//  Created by Joe on 2019/9/15.
-//  Copyright © 2019 Joe. All rights reserved.
+//  Created by Joe on 2020/5/14.
+//  Copyright © 2020 Joe. All rights reserved.
 //
 
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 struct num{
-    int r; // remainder
-    int n; // dividend
-    int flag; // to judge if the remainder is odd or even
+    int n;      // number itself
+    int r;      // remainder
+    int odd;   // 0: even, 1: odd
 }number[10000];
 
-bool compare(num A, num B){
-    if(A.r != B.r)
-        return A.r < B.r;
-    
-    else if (A.flag + B.flag == 1) // one odd, one even
-        return A.flag;
-    
-    else if(A.flag + B.flag == 2) // both odd
-        return A.n > B.n;
-    
-    return A.n < B.n; // both even
+bool compare(num a , num b){
+    if(a.r != b.r) return a.r < b.r;                // small remainder first
+    else if(a.odd + b.odd == 1) return a.odd;       // one odd one even
+    else if (a.odd + b.odd == 0) return a.n < b.n;  // two even
+    else return a.n > b.n;                          // two odd
 }
 
 int main(int argc, const char * argv[]) {
-    int N, M ;
+    int N, M;
     while(cin >> N >> M && (N != 0 && M != 0)){
-        for(int i = 0; i < N ; i++){
-            cin >> number[i].n;
-            number[i].r = number[i].n % M;
-            number[i].flag = abs(number[i].n % 2);
+        int input;
+        for(int i = 0; i < N; i++){
+            cin >> input;
+            number[i].n = input;
+            number[i].r = input % M;
+            number[i].odd = abs(input % 2);
         }
+        
         sort(number, number+N, compare);
+        
         cout << N << " " << M << endl;
-            
-        for(int i = 0 ; i < N ; i++){
-            cout << number[i].n << endl;
-        }
+        for(int i = 0; i < N; i++) cout << number[i].n << endl;
     }
-    cout <<"0 0"<< endl;
+    cout << "0 0" << endl;
     return 0;
 }
